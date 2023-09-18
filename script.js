795c50eb4907f69a033ae2f8d598cbe354a6640e
Pedro Henrique Serafini <pserafini@cohabct.com.br>
2023-09-18 16:19:12 -0300

    Alteração da imagem modelo para a final, alterção de cor e bordas e ícone do WhasApp adicionado.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function menuShow() {
     let menuMobile = document.querySelector('.menu__mobile');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/icons8-excluir-30.png"
+        document.querySelector('.icon').src = "assets/icons8-excluir-35.png"
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "assets/icons8-cardápio-35.png"
